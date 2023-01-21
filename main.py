f8603e99162d9a7255cddac457bb88ca65a32390
Oluwaseun <tanimolaoluwaseun70@gmail.com>
2023-01-21 19:17:43 +0100

    routes: profile

--- main.py
+++ main.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def dashboard():
     return render_template('index.html')
 
+@app.route('/profile')
+def user_profile():
+    return render_template('users-profile.html')
+
 if __name__ == '__main__':
     app.run()
